304b8fe3fae614d8ba752a01d9dfb91a36a81056
michal63178 <73548741+michal63178@users.noreply.github.com>
2021-10-18 17:54:10 +0200

    Add files via upload

---
+++ lab 1/6.java
@@ -0,0 +1,12 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String robot = "  /////\n +\"\"\"\"\"+\n(| o o |"
+                     + ")\n  | ^ |\n | `-' |\n +-----+";
+
+        System.out.print(robot);
+
+    }
+}
