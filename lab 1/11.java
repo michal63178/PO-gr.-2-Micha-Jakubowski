1ffed9ed8f36bad1a951663e4b697ed7a32539fd
michal63178 <73548741+michal63178@users.noreply.github.com>
2021-10-22 13:32:53 +0200

    Add files via upload

---
+++ lab 1/11.java
@@ -0,0 +1,49 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String poem = "Wolę kino.\n"
+                    + "Wolę koty.\n"
+                    + "Wolę dęby nad Wartą.\n"
+                    + "Wolę Dickensa od Dostojewskiego.\n"
+                    + "Wolę siebie lubiącą ludzi\n"
+                    + "niż siebie kochającą ludzkość.\n"
+                    + "Wolę mieć w pogotowiu igłę z nitką.\n"
+                    + "Wolę kolor zielony.\n"
+                    + "Wolę nie twierdzić,\n"
+                    + "że rozum jest wszystkiemu winien.\n"
+                    + "Wolę wyjątki.\n"
+                    + "Wolę wychodzić wcześniej.\n"
+                    + "Wolę rozmawiać z lekarzami o czymś innym.\n"
+                    + "Wolę stare ilustracje w prążki.\n"
+                    + "Wolę śmieszność pisania wierszy\n"
+                    + "od śmieszności ich niepisania.\n"
+                    + "Wolę w miłości rocznice nieokrągłe,\n"
+                    + "do obchodzenia na co dzień.\n"
+                    + "Wolę moralistów,\n"
+                    + "którzy nie obiecują mi nic.\n"
+                    + "Wolę dobroć przebiegłą od łatwowiernej za bardzo.\n"
+                    + "Wolę ziemię w cywilu.\n"
+                    + "Wolę kraje podbite niż podbijające.\n"
+                    + "Wolę mieć zastrzeżenia.\n"
+                    + "Wolę piekło chaosu od piekła porządku.\n"
+                    + "Wolę bajki Grimma od pierwszych stron gazet.\n"
+                    + "Wolę liście bez kwiatów niż kwiaty bez liści.\n"
+                    + "Wolę psy z ogonem nie przyciętym.\n"
+                    + "Wolę oczy jasne, ponieważ mam ciemne.\n"
+                    + "Wolę szuflady.\n"
+                    + "Wolę wiele rzeczy, których tu nie wymieniłam,\n"
+                    + "od wielu również tu nie wymienionych.\n"
+                    + "Wolę zera luzem\n"
+                    + "niż ustawione w kolejce do cyfry.\n"
+                    + "Wolę czas owadzi od gwiezdnego.\n"
+                    + "Wolę odpukać.\n"
+                    + "Wolę nie pytać jak długo jeszcze i kiedy.\n"
+                    + "Wolę brać pod uwagę nawet tę możliwość,\n"
+                    + "że byt ma swoją rację.";
+
+        System.out.print(poem);
+
+    }
+}
